63ca92c25c7d0aa82c733c485c3f8ed66752a9ea
Emie-Trialer <emiedonmukumo@gmail.com>
2024-04-14 02:08:45 +0100

    Updated the User Interface

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -26,8 +26,13 @@ const Countdown = ({targetDate}) =>{
 
     return(
         <div>
-            {countBoolean && <h1>{days}:{hours}:{minute}:{second}</h1>}
-            {showButton && <button onClick={count}>Start Counting</button>}
+            <div className="count-container">
+                    <h1><span className="count-time">{days}</span> Days</h1>
+                    <h1 ><span className="count-time">{hours}</span> Hours</h1>
+                    <h1 ><span className="count-time">{minute}</span> Minutes</h1>
+                    <h1 ><span className="count-time">{second}</span> Seconds</h1>
+                </div>
+            {showButton ? <button className="btn-secondary" onClick={count}>Start Counting</button> : <button className="btn-secondary" onClick={()=> window.location.reload()}>Refresh</button>}
         </div>
     )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -9,21 +9,8 @@ const Form = () =>{
     const [second, setSecond] = useState()
     const [targetDate, setTargetDate] = useState()
     
-    
     const handleSubmit = (e) =>{
         e.preventDefault()
-
-        const undefined_error = (value) =>{
-            if(value === undefined) {
-                alert(`${value} is required`);
-                return;
-            }
-        }
-        
-        undefined_error(date)
-        undefined_error(hour)
-        undefined_error(minute)
-        undefined_error(second)
 
         const formatting_time = (value) =>{
             if(value === undefined) {
@@ -39,34 +26,38 @@ const Form = () =>{
         }
         
         setTargetDate(new Date(`${date}T${formatting_time(hour)}:${formatting_time(minute)}:${formatting_time(second)}`))
-
         setShowForm(!showForm)
     }
 
     return(
         <div className="container">
-            <h1>Countdown Timer</h1>
-            {showForm ? (<form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="date">Date</label>
-                    <input type="date" name="date" id="" value={date} onChange={(e)=>setDate(e.target.value)}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="hour">Hour</label>
-                    <input type="number" name="hour" id="" value={hour} onChange={(e)=>setHour(e.target.value)} max="23"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="minute">Minute</label>
-                    <input type="number" name="minute" id="" value={minute} onChange={(e)=>setMinute(e.target.value)}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="second">Second</label>
-                    <input type="number" name="second" id="" value={second} onChange={(e)=>setSecond(e.target.value)}/>
-                </div>
-                <button>Submit</button>
-            </form>) : ''}
-            {showForm == false && <Countdown targetDate={targetDate}/> }
-            {showForm == false && <button onClick={()=>window.location.reload()}>Refresh</button> }
+            <div className="form-flex">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-container">
+                        <div className="form-group">
+                            <label htmlFor="hour">Hour</label>
+                            <input type="number" name="hour" id="" value={hour} onChange={(e)=>setHour(e.target.value)} max="23"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="minute">Minute</label>
+                            <input type="number" name="minute" id="" value={minute} onChange={(e)=>setMinute(e.target.value)}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="second">Second</label>
+                            <input type="number" name="second" id="" value={second} onChange={(e)=>setSecond(e.target.value)}/>
+                        </div>
+                    </div>
+                    <div className="form-date">
+                        {/* <label htmlFor="date">Date</label> */}
+                        <input type="date" name="date" id="" value={date} onChange={(e)=>setDate(e.target.value)}/>
+                    </div>
+                    <div class="flex align-center divider">
+                        <span class="hr"></span>
+                        <button class="btn-secondary" id="submit" style={{width: '20%'}}>Update</button>
+                    </div>
+                </form> 
+            </div>
+            <Countdown targetDate={targetDate}/>
         </div>
     )
 }
